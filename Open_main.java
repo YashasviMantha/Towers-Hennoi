b68aabf3646b7fafec72c78fa9ca300c4830d300
Yashasvi Mantha <yashasvimantha@gmail.com>
2019-03-19 00:41:38 +0530

    Major Update
    Fixed the frame being displayed in the corner. 
    Added 2 more buttons.
    Added space in between the buttons. 
    Changed the font and the size of the button.

--- Open_main.java
+++ Open_main.java
@@ -4,12 +4,12 @@ import java.awt.event.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-class Open_main implements ActionListener
+class Open_main
 {
     Open_main()
     {
        JFrame MenuFrame = new JFrame("Menu - Towers Of Hennoi");
-        MenuFrame.setSize(800, 800);
+        MenuFrame.setSize(500, 800);
         GridBagLayout layout = new GridBagLayout();
         MenuFrame.setLayout(layout);
 
@@ -18,18 +18,33 @@ class Open_main implements ActionListener
 
 
         JButton B1 = new JButton("Start Simulation!");
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        B1.setToolTipText("Click to start simulation");
-        B1.setActionCommand("Sim_St");
-//        B1.addActionListener(this);
-        //gbc.gridx = 0;
-//        gbc.weightx = 1.0;
-//        gbc.weighty = 1.0;
-//        gbc.gridy = 2;
+        B1.setToolTipText("Click to start simulation!");
+        B1.setPreferredSize(new Dimension(300, 60));
+        B1.setFont(new Font("Arial", Font.PLAIN, 28));
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.insets = new Insets(10,10,10,10);
         MenuFrame.add(B1,gbc);
 
+        JButton B2 = new JButton("Rules");
+        B2.setToolTipText("Click to understand the rules!");
+        B2.setFont(new Font("Arial", Font.PLAIN, 28));
+        B2.setPreferredSize(new Dimension(300, 60));
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        MenuFrame.add(B2,gbc);
+
+        JButton B3 = new JButton("Credits");
+        B3.setToolTipText("Click for Credits!");
+        B3.setFont(new Font("Arial", Font.PLAIN, 28));
+        B3.setPreferredSize(new Dimension(300, 60));
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        MenuFrame.add(B3,gbc);
 
 
+        //MenuFrame.pack();                     //Packes the size of the frame just so that all the buttons fit.
+        MenuFrame.setLocationRelativeTo(null);  //Places the frame to the middle of the screen
         MenuFrame.setVisible(true);
         //MenuFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
